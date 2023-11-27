5ea554f74f13193d0cbd1bd433548aa815ad3276
VŨ NGUYỄN MINH TIẾN <petrustien@192.168.2.16>
2023-11-27 21:33:17 +0700

    connect DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const dbConnect = require('./src/config/dbConnect');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const pORT = process.env.PORT || 5001
 
 const app = express();
 app.use(express.json()) // data client gửi lên đọc theo kiểu json
 app.use(express.urlencoded({extended : true})) // data client gửi lên đọc theo kiểu urlencoded (array...)
+dbConnect();
 
 app.listen(pORT, () => {
     console.log("Server is listenning on http://localhost:" + pORT);
---
+++ server/src/config/dbConnect.js
@@ -0,0 +1,18 @@
+const { default : mongoose} = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const linkDB = process.env.MONGO_URL
+
+const connectToDB = async () => {
+    try {
+        const conn = await mongoose.connect(linkDB);
+        if (conn.connection.readyState === 1) console.log("Connection to DB is successful")
+        else console.log("DB is connecting");
+    } catch (error) {
+        console.log("Connection to DB is failed");
+        throw new Error(error);
+    }
+};
+
+module.exports = connectToDB;
