4343f9ec6666e1abb52d4789e030744eede34e88
VŨ NGUYỄN MINH TIẾN <khoatien158@gmail.com>
2024-04-06 21:53:53 +0700

    delete product BE

--- server/src/routers/product.js
+++ server/src/routers/product.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { createProduct, getProduct, getAllProducts, updateProduct } = require('../controllers/product');
+const { createProduct, getProduct, getAllProducts, updateProduct, deleteProduct } = require('../controllers/product');
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken');
 
 router.post("/", [verifyAccessToken, isAdmin], createProduct);
-router.put("/:pid", [verifyAccessToken, isAdmin], updateProduct);
 router.get("/", getAllProducts);
+
+router.put("/:pid", [verifyAccessToken, isAdmin], updateProduct);
+router.delete("/:pid", [verifyAccessToken, isAdmin], deleteProduct);
 router.get("/:pid", getProduct);
 
 
